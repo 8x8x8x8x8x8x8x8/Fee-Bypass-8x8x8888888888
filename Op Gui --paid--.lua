cb4f6480b5cc6da7864715accc09af2df0e82673
8x8x8x8x8x8x8x8 <pkrottinghaus@gmail.com>
2026-09-24 18:30:14 -0500

    Update Op Gui --paid--.lua

--- Op Gui --paid--.lua
+++ Op Gui --paid--.lua
@@ -3,16 +3,24 @@
 
 -- Instances:
 
+local MarketplaceService = game:GetService("MarketplaceService")
 local Players = game:GetService("Players")
 local StarterGui = game:GetService("StarterGui")
 
 local player = Players.LocalPlayer
-local SHIRT_ID = "17745689249"
+local SHIRT_ID = 17745689249
 
-local character = player.Character or player.CharacterAdded:Wait()
-local shirt = character:FindFirstChildOfClass("Shirt")
+local ownsShirt = false
 
-if not shirt or not string.find(shirt.ShirtTemplate, SHIRT_ID) then
+local success, result = pcall(function()
+	return MarketplaceService:PlayerOwnsAssetAsync(player, SHIRT_ID)
+end)
+
+if success then
+	ownsShirt = result
+end
+
+if not ownsShirt then
 	StarterGui:SetCore("SendNotification", {
 		Title = "Access Denied",
 		Text = "You need the shirt!",
