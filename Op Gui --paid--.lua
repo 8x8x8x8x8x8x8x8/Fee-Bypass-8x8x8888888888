89f42d84870d4b70357001ba33a25c7b1312802e
8x8x8x8x8x8x8x8 <pkrottinghaus@gmail.com>
2026-09-24 19:04:51 -0500

    Update Op Gui --paid--.lua

--- Op Gui --paid--.lua
+++ Op Gui --paid--.lua
@@ -3,15 +3,13 @@ local StarterGui = game:GetService("StarterGui")
 
 local player = Players.LocalPlayer
 
--- ADD PROFILE/USER IDs HERE
+-- PUT YOUR ROBLOX PROFILE/USER ID HERE
 local Whitelist = {
 	[6113961118] = true,
-	[6111640647] = true,
 	[] = true,
 }
 
--- WHITELIST CHECK
-if not Whitelist[player.UserId] then
+if Whitelist[player.UserId] ~= true then
 	pcall(function()
 		StarterGui:SetCore("SendNotification", {
 			Title = "Whitelist",
@@ -23,7 +21,9 @@ if not Whitelist[player.UserId] then
 	return
 end
 
--- YOUR EXISTING SCRIPT STARTS HERE
+print("WHITELIST PASSED!")
+
+-- EVERYTHING ELSE IN YOUR SCRIPT GOES BELOW THIS
 
 -- Gui to Lua
 -- Version: 3.2
